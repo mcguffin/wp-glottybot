74a9d7968d62b8fc3d6e2ef003f03feb257f62af
mcguffin <joern@flyingletters.com>
2014-12-24 14:31:04 +0100

    submod

--- css/flag-icon-css/build.py
+++ css/flag-icon-css/build.py
@@ -2,84 +2,79 @@
 
 import sys, os, pystache, urllib, pprint
 
-wp_codes = ['bg','ba','dk','de','ca','gb','au','es','pe','cl','ir','fr','es','il','hu','id','it','kr','mm','no','nl','pl','pt','br','ru','sk','rs','se','tr','cn','tw','us']
-# contries covered by wp localizations (reonstructed from WP-Langcodes without countrycode)
-# langcode	Country
-#	'ar'	'eg','dz','bh','dj','er','iq','il','ye','jo','qa','km','kw','lb','ly','ma','mr','om','sa','so','sd','sy','td','tn','ae',
-#	'az'	'az',
-#	'ca'	'fr',
-#	'cy'	'gb',
-#	'eu'	'es',
-#	'fi'	'fi',
-#	'gd'	'gb',
-#	'hr'	'hr',
-#	'ja'	'jp',
-#	'th'	'th',
-# known languages having more than one country
-#	'de'	'ch','at','be'
-#	
-
 do_codes = []
 
 css_template = """
 
-.i18n-item {
-	position: relative !important;
-	width: 1.3333333333333333em !important;
-	height:1em !important;
-	margin-right:0.5em !important;
+
+.i18n-item[data-country][lang],
+.i18n-item[data-country][data-language] {
+	position:relative;
 }
 
-.i18n-item[data-language],
-.i18n-item[data-country] {
-	position: relative;
+.i18n-item[lang]:after,
+.i18n-item[data-language]:after,
+.i18n-item[data-country]:before {
+	content:' ';
 	display: inline-block;
 	width: 1.3333333333333333em;
 	height:1em;
-	line-height: 1em;
-	background-size: contain;
-	background-position: 50%;
+	background-size: 1.3333333333333333em 1em;
+	background-position: center center;
 	background-repeat: no-repeat;
 }
-.i18n-item[data-language] {
-	border:1px solid rgba(30,30,30,0.8);
-	background-color:rgba(255,255,255,0.9);
+.i18n-item[data-country][data-language]:before,
+.i18n-item[data-country][lang]:before {
+	margin-right:0.5em;
 }
-.i18n-item[data-language][data-country] {
-	border-style:none;
-	background-color:transparent;
-}
-
+.i18n-item[lang]:after,
 .i18n-item[data-language]:after {
-	position:absolute;
-	right:0;
-	left:0;
-	bottom:0;
-	content:attr(data-language);
 	text-align:center;
 	text-transform:uppercase;
 	font-size: 0.8em;
-	line-height:1.2em;
+	line-height:1.0em;
 	font-weight:bold;
 	color:#000;
 	background-color:transparent;
+	border:1px solid rgba(30,30,30,0.8);
+	background-color:rgba(255,255,255,0.9);
+	width: 1.3333333333333333em;
+	height:1em;
+	padding:0.0125em 0.125em;
+	box-sizing:content-box;
 }
-.i18n-item[data-language][data-country]:after {
-	left:auto;
-	padding:0 0.1em;
-	font-size: 0.6em;
-	background:rgba(255,255,255,0.9);
+.i18n-item.invert:after {
+	color:#fff;
+	border-style:none;
+	background-color:rgba(0,0,0,0.65);
+}
+.i18n-item[data-country][lang]:after,
+.i18n-item[data-country][data-language]:after {
+	position:absolute;
+	left:1.1em;
+	top:0.8em;
+	font-size:0.6em;
+	width: auto;
+	height:auto;
+}
+.i18n-item[lang]:after {
+	float:left;
+}
+.i18n-item[data-country][lang]:after {
+	float:none;
 }
 
-.i18n-item[data-language].invert:after {
-	color:#fff;
-	background:rgba(0,0,0,0.6);
+.i18n-item[lang]:after {
+	content:attr(lang);
+}
+.i18n-item[data-language]:after {
+	content:attr(data-language);
 }
 
 
 {{#country_codes}}
-.i18n-item[data-country='{{country_code_lower}}'],
-.i18n-item[data-country='{{country_code_upper}}'] {
+.i18n-item[data-country='{{country_code_lower}}']:before,
+.i18n-item[data-country='{{country_code_upper}}']:before {
 	background-image:url( '../flags/4x3/{{country_code_lower}}.svg' );
 }
 {{/country_codes}}
